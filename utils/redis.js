ba76c3c149ae94306ed66d8a9ca7e6ab8819f5a6
ochuks1 <cochuks@gmail.com>
2025-01-07 15:20:35 +0000

    Correcting indentation

--- utils/redis.js
+++ utils/redis.js
@@ -2,38 +2,38 @@ import redis from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
+  constructor() {
+    this.client = redis.createClient();
 
-        this.client.on('error', (err) => {
-            console.error(`Redis client error: ${err}`);
-        });
+    this.client.on('error', (err) => {
+      console.error(`Redis client error: ${err}`);
+    });
 
-        this.client.on('connect', () => {
-            console.log('Redis client connected');
-        });
+    this.client.on('connect', () => {
+      console.log('Redis client connected');
+    });
 
-        this.getAsync = promisify(this.client.get).bind(this.client);
-        this.setAsync = promisify(this.client.set).bind(this.client);
-        this.delAsync = promisify(this.client.del).bind(this.client);
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    try {
+      return await this.getAsync(key);
+    } catch (err) {
+      console.error(`Error getting key ${key}: ${err}`);
+      return null;
     }
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
-
-    async get(key) {
-        try {
-            return await this.getAsync(key);
-        } catch (err) {
-            console.error(`Error getting key ${key}: ${err}`);
-            return null;
-        }
-    }
-
-    async set(key, value, duration) {
-        try {
-            await this.setAsync(key, value);
+  async set(key, value, duration) {
+    try {
+      await this.setAsync(key, value);
             this.client.expire(key, duration);
         } catch (err) {
             console.error(`Error setting key ${key}: ${err}`);
